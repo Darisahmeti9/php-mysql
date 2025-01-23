4ef08f13ef5e13a0fbe5d9e98a501678e82a6a43
Darisahmeti9 <166027089+Darisahmeti9@users.noreply.github.com>
2025-01-23 16:18:48 +0100

    dar

---
+++ moduli2/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$emri= "daris" ;
+$mosha= 20;
+$pesha= 4.5;
+
+echo $emri;
+echo $mosha;
+echo $pesha;
+
+$x=134;
+$y=152;
+
+echo $x + $y;
+
+$fjalia = "I love PHP";
+echo str_replace ("love", "dont love", $fjalia);
+echo strlen($fjalia);
+
+?>
