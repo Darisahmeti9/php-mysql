4688372d3b8acca36fdd6cf3948d0c9c9f45e02b
Darisahmeti9 <166027089+Darisahmeti9@users.noreply.github.com>
2025-01-28 16:16:43 +0100

    dae

---
+++ moduli3/index.php
@@ -0,0 +1,47 @@
+<?php 
+
+ $score=100;
+
+ if($score>=50){
+    echo  "YOU GOT A 1";
+ }
+
+ if($score>=69){
+    echo  "YOU GOT A 2";
+ }
+
+ if($score>=79){
+    echo  "YOU GOT A 3";
+ }
+
+ if($score>=89){
+    echo  "YOU GOT A 4";
+ }
+
+ if($score>=100){
+    echo  "YOU GOT A 5";
+ }
+
+
+ $day="Tuesday";
+
+ switch ($day) {
+   case 'Monday':
+      "echo nuk ki kurs";
+      break;
+   
+   default:
+      # code...
+      break;
+      case 'Thruday':
+      "echo nuk ki kurs";
+       break;
+      case 'tuesday':
+      "echo nuk ki kurs";
+       break;
+      case 'Friday':
+      "echo nuk ki kurs";
+      break;
+ }
+
+?>
