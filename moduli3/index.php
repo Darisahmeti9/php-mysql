cdb79fe5401af6f487af9042aae67e29e665b110
Darisahmeti9 <166027089+Darisahmeti9@users.noreply.github.com>
2025-01-30 16:15:22 +0100

    dar

--- moduli3/index.php
+++ moduli3/index.php
@@ -44,4 +44,26 @@
       break;
  }
 
+
+
+ $x=50
+
+ for($x=50 ; $x<=100 ; $x+=2 ;){
+   echo "$x <br>";
+ }
+
+$students = array("Daris", "Puhiza", "Anik", "Andi", "Eden", "Selatin", "Bled", "Nil", "Blin", "Erion", "Drorant", "Olsa");
+
+foreach ($students as $x);{
+   echo "$x <br>";
+}
+
+$string= 'darisA';
+
+while(strlen($string) >10){
+echo ($string);
+$strinng=$string. 'd';
+}
+
 ?>
+
