443b9deba4aa15a28a3b2693672b99846f0cc3e0
Darisahmeti9 <166027089+Darisahmeti9@users.noreply.github.com>
2025-02-06 16:16:41 +0100

    daris

--- moduli4/index.php
+++ moduli4/index.php
@@ -1,32 +1,62 @@
 <?php
-function findmax($nr1, $nr2){
-    if($nr1 > $nr2){
-        return $nr1;
-    }
-    else{
-        return $nr2;
-    }
+// function findmax($nr1, $nr2){
+//     if($nr1 > $nr2){
+//         return $nr1;
+//     }
+//     else{
+//         return $nr2;
+//     }
+// }
+// echo findmax(15,30);
+
+
+// function daris($nrr1){
+//     if($nrr1%2==0){
+//         echo "$nrr1 this number is ever";
+//     }
+//     else{
+//         echo "$nrr1 this numer is old";
+//     }
+// }
+
+// daris(6);
+
+// echo "<br>";
+
+// $sum = 963475450952943 + 427389494233;
+
+// echo $sum;
+
+// echo "<br>";
+
+$students;
+
+$count= count($students);
+
+$students=["Daris","Anik","Andi","Shpat"];
+
+for($i=0; $i<= $count; $i++;){
+    $students[i];
 }
-echo findmax(15,30);
 
+echo "br";
 
-function daris($nrr1){
-    if($nrr1%2==0){
-        echo "$nrr1 this number is ever";
-    }
-    else{
-        echo "$nrr1 this numer is old";
-    }
-}
+array_push($sport , 'Andi');
 
-daris(6);
+print_r($students);
 
-echo "<br>";
+array_unshift($students, 'Anik');
 
-$sum = 963475450952943 + 427389494233;
+print_r($students);
 
-echo $sum;
+array_pop($studesnts);
 
-echo "<br>";
+print_r($students);
+
+array_shift($students,);
+
+print_r($students);
+
+$sport_slice= array_slice();
 
 ?>
