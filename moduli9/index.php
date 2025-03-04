5a5ab41297ebe93a7e81b4215ce2989b56b2f80a
Darisahmeti9 <166027089+Darisahmeti9@users.noreply.github.com>
2025-03-04 16:17:40 +0100

    daaaa

---
+++ moduli9/index.php
@@ -0,0 +1,28 @@
+<?php
+
+    $username = $_GET['username'];
+    $password = $_GET['password'];
+    echo $username;
+    echo "<br>";
+    echo $password;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>From</title>
+</head>
+<body>
+    <from action="index.php" method="get">
+
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" placeholder="Username"><br>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password" placeholder="Password"><br>
+    <input type="submit" value="Submit">
+    </from>
+    
+</body>
+</html>
